1c8a110bee6509423007a0f8d661af4a99b8e2e3
Bhagwat Munde <bmunde031@gmail.com>
2025-07-07 18:29:37 +0530

    railway port

--- app.py
+++ app.py
@@ -258,7 +258,11 @@ def show_mindmap():
 
 if __name__ == '__main__':
     # app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    # app.run(host='0.0.0.0', port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
     # good day 
